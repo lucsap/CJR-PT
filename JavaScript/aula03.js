acd39c23a3bdc3975eab5efc7504fa6b75e80bc5
Lucas Pereira <lucaspework@gmail.com>
2021-09-20 18:18:50 -0300

    Update aula03.js

--- JavaScript/aula03.js
+++ JavaScript/aula03.js
@@ -1,14 +1,14 @@
-let animaisReserva = ["tubarao", "cobra"]
+// let animaisReserva = ["tubarao", "cobra"]
 
-let animais = ["Gato", "cao", "pato", "pinguin"]
+// let animais = ["Gato", "cao", "pato", "pinguin"]
 
-// console.log(animais.slice(0))
+// // console.log(animais.slice(0))
 
 
 
-let indice = animais.indexOf("pato")
+// let indice = animais.indexOf("pato")
 
-console.log(indice)
+// console.log(indice)
 
 // push - inserir no final
 // unshift - adicionar no início
@@ -17,3 +17,65 @@ console.log(indice)
 // slice - seleciona dentro de um intervalo
 // splice - remove a partir de um indice e uma quantidade
 // indexOf - retornar o indice de um determinado elemento
+// spread operator - (inclui os intens dew um vetor em outro)
+
+// for(let i = 0, i < 10, i++) {
+//     console.log(i)
+//     if(i == 5) {
+//         continue
+//     }
+
+//     console.log("Ele continuou depois do 5")
+// }
+
+// let i = 0
+
+// while() {
+//     console.log(i)
+//     i++
+// }
+
+// let notas = [10, 5.7, 8.9]
+
+// let sum = 0
+
+// for(let i = 0; i < 10; i++) {
+//     sum += notas[i]
+// }
+
+// let media = sum / 3
+
+// if(media > 7) {
+//     console.log(`Média = ${media}, o aluno aprovado`)
+// } else {
+//     console.log(`Média = ${media}, o aluno reprovado`)
+// }
+
+// let nomes = ["vanessa", "bruno", "tilnoene", "joao"]
+
+// let nome = "vanessa"
+
+// for(let nome of nomes) {
+//     console.log(nome)
+// }
+
+// let media = sum / 3
+
+// if(media > 7) {
+//     console.log(`Média = ${media}, o aluno aprovado`)
+// } else {
+//     console.log(`Média = ${media}, o aluno reprovado`)
+// }
+
+// var numbers = [1, 4, 9]
+// var roots = numbers.map(Math.sqrt)
+
+// document.write(roots)
+
+// var numeros = [1, 2, 3, 4, 5]; // vetor original
+// var quadrados = numeros.map(function(item){
+//     return Math.pow(item, 2); // retorna o item original el
+// });
+
+// document.write(quadrados); // imprime 1, 4, 9, 16, 25
+
