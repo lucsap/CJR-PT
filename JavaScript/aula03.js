f36c7253e568e51c6087527b714f5efee64f0ac4
Lucas Pereira <lucaspework@gmail.com>
2021-09-20 12:52:54 -0300

    aula 3

---
+++ JavaScript/aula03.js
@@ -0,0 +1,19 @@
+let animaisReserva = ["tubarao", "cobra"]
+
+let animais = ["Gato", "cao", "pato", "pinguin"]
+
+// console.log(animais.slice(0))
+
+
+
+let indice = animais.indexOf("pato")
+
+console.log(indice)
+
+// push - inserir no final
+// unshift - adicionar no início
+// pop - retirar no final
+// shift - retirar no início
+// slice - seleciona dentro de um intervalo
+// splice - remove a partir de um indice e uma quantidade
+// indexOf - retornar o indice de um determinado elemento
