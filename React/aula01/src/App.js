00d6ced9f519c2c1b104b4222948b79c99561ea9
Lucas Pereira <lucaspework@gmail.com>
2021-10-06 00:10:16 -0300

    react na prática

--- React/aula01/src/App.js
+++ React/aula01/src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Eu quero mudar de país</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Agora eu sou um Deus do universo, ajoelhem aos meus pés!
         </p>
         <a
           className="App-link"
